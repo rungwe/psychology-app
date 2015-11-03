891dd2f85d1b312c4a6e287d2d97b90342b0bb92
Chaddy <kumbirair@yahoo.com>
2015-11-03 16:18:49 +0200

    9

--- update.php
+++ update.php
@@ -4,7 +4,7 @@
  header("Content-type: text/plain"); 
  // Run the script - make sure that your SSH key is set with *no* password
  
-  shell_exec("./pull.sh"); 
+  shell_exec("/home/ubuntu/public_html/pull.sh"); 
 
 
 ?>
