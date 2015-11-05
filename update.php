af6dccf1f100afa04f7c98251e3a24e25e311492
Chaddy <kumbirair@yahoo.com>
2015-11-05 17:31:19 +0200

    deploy

--- update.php
+++ update.php
@@ -4,7 +4,9 @@
  header("Content-type: text/plain"); 
  // Run the script - make sure that your SSH key is set with *no* password
  
-  shell_exec("/home/ubuntu/public_html/pull.sh"); 
+  //echo shell_exec("/home/ubuntu/public_html/pull.sh"); 
+  echo "<br>";
+  echo exec("/home/ubuntu/public_html/pull.sh");
 
 
 ?>
