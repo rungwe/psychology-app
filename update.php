ac1739cee23c5d926c2b6a67832886efd12b7ba4
Chaddy <kumbirair@yahoo.com>
2015-11-05 18:49:07 +0200

    deployment fix

--- update.php
+++ update.php
@@ -4,7 +4,7 @@
  header("Content-type: text/plain"); 
  // Run the script - make sure that your SSH key is set with *no* password
  
-  var_dump( shell_exec("/usr/bin/php /home/ubuntu/public_html/update.php") ); 
+  //var_dump( shell_exec("/usr/bin/php /home/ubuntu/public_html/update.php") ); 
   echo "<br>";
   var_dump( exec("git pull --no-edit origin master") );
 
