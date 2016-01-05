3781df3c79091adcfad70dfdee1fbbe0c3655cf3
Silwana <azilesil@gmail.com>
2016-01-05 16:35:34 +0200

    edited email template

--- components.php
+++ components.php
@@ -803,25 +803,6 @@ body.outlook p {
                   </tr>
                 </table>
 
-                <table class="row" style="padding-bottom: 20px">
-                  <tr>
-                    <td class="wrapper last">
-
-                      <table class="twelve columns">
-                        <tr>
-                          <td align="center">
-                            <center>
-                              <p style="text-align:center;"><a href="#">Terms</a> | <a href="#">Privacy</a></p>
-                            </center>
-                          </td>
-                          <td class="expander"></td>
-                        </tr>
-                      </table>
-
-                    </td>
-                  </tr>
-                </table>
-
               <!-- container end below -->
               </td>
             </tr>
